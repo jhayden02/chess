66efe26971d5e9174f04339e280a0d1b4d5f22d0
Joshua Hayden <joshhayden.business@gmail.com>
2026-06-05 14:09:15 -0400

    Switched to using textures for the board and pieces.

--- src/chess.c
+++ src/chess.c
@@ -20,14 +20,15 @@
 
 #include "raylib.h"
 
-#define GAME_HEIGHT 800
-#define GAME_WIDTH 800
+#define GAME_HEIGHT 852
+#define GAME_WIDTH 852
 #define GAME_FPS 60
 
 #define BOARD_ROWS 8
-#define BOARD_COLS 8
-
-#define SQUARE_PIXELS 100
+#define BOARD_COLS BOARD_ROWS
+#define BOARD_SQUARE_SIZE 96
+#define BOARD_X_OFFSET 42
+#define BOARD_Y_OFFSET (BOARD_SQUARE_SIZE + (BOARD_SQUARE_SIZE / 2))
 
 typedef enum {
     SQUARE_EMPTY,
@@ -48,47 +49,79 @@ typedef enum {
 square board[BOARD_ROWS][BOARD_COLS];
 
 // White pieces are uppercase, black pieces are lowercase.
-void init_board() {
+void init_board()
+{
     for (int col = 0; col < BOARD_COLS; col++) {
-        board[col][6] = SQUARE_BLACK_PAWN;
-        board[col][1] = SQUARE_WHITE_PAWN;
+        board[1][col] = SQUARE_WHITE_PAWN;
+        board[6][col] = SQUARE_BLACK_PAWN;
     }
-}
+    
+    board[0][0] = SQUARE_WHITE_ROOK;
+    board[0][7] = SQUARE_WHITE_ROOK;
 
-void draw_board()
-{
-    const int cols = GAME_WIDTH / SQUARE_PIXELS;
-    const int rows = GAME_HEIGHT / SQUARE_PIXELS;
+    board[7][0] = SQUARE_BLACK_ROOK;
+    board[7][7] = SQUARE_BLACK_ROOK;
 
-    for (int y = 0; y < rows; y++) {
-        for (int x = 0; x < cols; x++) {
-            const bool is_dark = (x + y) % 2;
-            const Color color = is_dark ? BLACK : RAYWHITE;
-            const float draw_x = x * SQUARE_PIXELS;
-            const float draw_y = y * SQUARE_PIXELS;
-            DrawRectangle(draw_x, draw_y, SQUARE_PIXELS, SQUARE_PIXELS, color);
-        }
-    }
-}
+    board[0][1] = SQUARE_WHITE_KNIGHT;
+    board[0][6] = SQUARE_WHITE_KNIGHT;
+    
+    board[7][1] = SQUARE_BLACK_KNIGHT;
+    board[7][6] = SQUARE_BLACK_KNIGHT;
+    
+    board[0][2] = SQUARE_WHITE_BISHOP;
+    board[0][5] = SQUARE_WHITE_BISHOP;
+    
+    board[7][2] = SQUARE_BLACK_BISHOP;
+    board[7][5] = SQUARE_BLACK_BISHOP;
 
-void update_board()
-{
+    board[0][3] = SQUARE_WHITE_QUEEN;
+    board[0][4] = SQUARE_WHITE_KING;
+    
+    board[7][3] = SQUARE_BLACK_QUEEN;
+    board[7][4] = SQUARE_BLACK_KING;
 }
 
 int main(void)
 { 
-    InitWindow(GAME_WIDTH, GAME_HEIGHT, "chess");
+    InitWindow(GAME_WIDTH, GAME_HEIGHT, "Chess");
+
+    Image i_chess_board = LoadImage("res/board.png");
+    Image i_pieces = LoadImage("res/pieces.png");
+
+    Texture2D t_chess_board = LoadTextureFromImage(i_chess_board);
+    Texture2D t_pieces = LoadTextureFromImage(i_pieces);
+
+    UnloadImage(i_chess_board);
+    UnloadImage(i_pieces);
+
     SetTargetFPS(GAME_FPS);
 
     init_board();
 
     while (!WindowShouldClose())
     {
-        update_board();
-
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        draw_board();
+        
+        // Draw board and pieces.
+        DrawTexture(t_chess_board, 0, 0, WHITE);
+        for (int row = 0; row < BOARD_ROWS; row++) {
+            for (int col = 0; col < BOARD_COLS; col++) {
+                const square current_square = board[row][col];
+                const Rectangle piece_rec = {
+                    current_square * BOARD_SQUARE_SIZE,
+                    0.0f,
+                    BOARD_SQUARE_SIZE,
+                    BOARD_SQUARE_SIZE
+                };
+                const Vector2 position = {
+                    (col * BOARD_SQUARE_SIZE) + BOARD_X_OFFSET,
+                    GAME_HEIGHT - (row * BOARD_SQUARE_SIZE) - BOARD_Y_OFFSET
+                };
+                DrawTextureRec(t_pieces, piece_rec, position, WHITE);
+            }
+        }
+
         EndDrawing();
     }
 
